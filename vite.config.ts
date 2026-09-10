f2898aa15ec35b834635ef66317415f5f039fd2a
tylerjhendricks95-cpu <tyler.j.hendricks95@gmail.com>
2026-09-10 14:39:46 -0400

    Update base path in vite.config.ts
    Changed base path to relative for iframe compatibility.

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Owlbear-Tracker/',
+  base: './', // Using relative base prevents broken script loading inside iframes!
 });
